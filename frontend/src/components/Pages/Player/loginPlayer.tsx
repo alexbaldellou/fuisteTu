5cd0bbdd859afaf0253481cd4dd4cde15051081b
lobo772 <59614268+lobo772@users.noreply.github.com>
2025-07-08 10:32:19 +0200

    fix deploy

--- frontend/src/components/Pages/Player/loginPlayer.tsx
+++ frontend/src/components/Pages/Player/loginPlayer.tsx
@@ -6,10 +6,13 @@ interface LoginPlayerInter {
 }
 const LoginPlayer = (props: LoginPlayerInter) => {
   const { onChangePlayer } = props;
+  const oleborrar = (si: boolean) => {
+    console.log(si);
+  };
 
   return (
     <div className="w-full flex justify-center items-center flex-col h-dvh bg-gradient-to-tr from-pink-500 to-yellow-500">
-      <RegisterPlayer onChangePlayer={onChangePlayer} />
+      <RegisterPlayer onChangePlayer={onChangePlayer} onSave={oleborrar} />
     </div>
   );
 };
