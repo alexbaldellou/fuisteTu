900f8629a0d41273b5184096523b7fc312e4870b
lobo772 <59614268+lobo772@users.noreply.github.com>
2025-07-21 10:14:55 +0200

    cloudflare

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,7 +1,8 @@
 
 import { io } from "socket.io-client";
+const socket = io("https://duncan-apply-collar-proof.trycloudflare.com", {
 // const socket = io("http://localhost:3000", {
-const socket = io("https://fuistetuback.onrender.com", {
+// const socket = io("https://fuistetuback.onrender.com", {
     transports: ['websocket'], // forzar WebSocket
   withCredentials: true,
 }); // o tu backend
